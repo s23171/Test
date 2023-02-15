e32e152e615f592a0cfe30c6da58eef4e4be64df
s23171 <101318811+s23171@users.noreply.github.com>
2023-02-15 23:44:57 +0100

    Add files via upload

---
+++ JavaProjects/TestowanieGitHubVol3/src/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+        printHelloWorld();
+        System.out.println("HelloWorldAgain");
+    }
+    public static void printHelloWorld(){
+        System.out.println("HelloWorld");
+    }
+
+}
